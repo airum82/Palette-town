182fdda1b9004ed7bbe06b1c0b30f6c2dce731f2
Arram Mandel <airum82@gmail.com>
2018-08-13 16:31:53 -0600

    Setup random color generation

---
+++ public/js/scripts.js
@@ -0,0 +1,22 @@
+const addColors = () => {
+  let color = '#';
+  const letters = '0123456789ABCDEF';
+  for(let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color
+}
+
+const createPalette = (() => {
+  for(let i = 1; i < 6; i++) {
+    $('.color-palette').append(
+      `<div class=${'color' + i}>
+        <h3></h3>
+      </div>`
+    );
+    const color = addColors();
+    $(`.color${i}`).css('background-color', color);
+    $(`.color${i}`).children('h3').html(color);
+  }
+  $('.color-palette div').addClass('color');
+})();
