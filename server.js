7cdae9e422bf66a968c4d64c1dad3335fe35b1f2
Arram Mandel <airum82@gmail.com>
2018-08-20 08:54:45 -0600

    Added comments to server file

--- server.js
+++ server.js
@@ -18,11 +18,14 @@ app.locals.folders = []
 app.post('/api/v1/newFolder', (request, response) => {
   const { name } = request.body;
   if(!name) {
+    //user must have a palette name to submit
+    //returns message saying they must have a name
     return response.status(401).send(new Error({
       message: 'no folder name provided'
     }))
   } else {
-    app.locals.folders.push(request.body)
+    //inserts new project into projects folder
+    //then sends response saying that new folder was added
     database('projects').insert(request.body, 'id')
       .then((project) => response.status(200).send({
         message: 'new folder added'
@@ -32,10 +35,13 @@ app.post('/api/v1/newFolder', (request, response) => {
 
 app.put('/api/v1/folders/:id', (request, response) => {
   if(request.body === {}) {
+    //sends error message when no palette info provided
     return response.status(422).send({
       error: 'no palette provided'
     })
   } else {
+    //inserts new palette into palettes table
+    //tells postgres to create Id for record
     database('palettes').insert(request.body, 'id')
       .then(palette => response.status(200).send({ message: 
         'new palette added to project'}))
@@ -43,6 +49,8 @@ app.put('/api/v1/folders/:id', (request, response) => {
 })
 
 app.get('/api/v1/folders', (request, response) => {
+  //fetches all projects from project table in palettetown database
+  //sends them to the front end
   return database('projects').select()
     .then((projects) => {
       return response.status(200).json(projects)
@@ -53,6 +61,8 @@ app.get('/api/v1/folders', (request, response) => {
 })
 
 app.get('/api/v1/palettes', (request, response) => {
+  //fetches all palettes from palettes table in database
+  //after fetching, sends palettes to front end
   return database('palettes').select()
     .then(palettes => {
       return response.status(200).json(palettes)
@@ -60,6 +70,7 @@ app.get('/api/v1/palettes', (request, response) => {
 })
 
 app.delete('/api/v1/delete/palette', (request, response) => {
+  //removes specific palette from database by matching foreign key
   return database('palettes').select()
     .where('project_id', request.body.project_id)
     .where('name', request.body.paletteName)
